21df9f94a3d94461ead7fda75440b7d8fd1c90e7
Aneesh-Pothuru <54965062+Aneesh-Pothuru@users.noreply.github.com>
2020-05-28 17:37:28 -0700

    Added navigation bar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./components/navBar/navBar";
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundColor: "#51ff81" }}>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
